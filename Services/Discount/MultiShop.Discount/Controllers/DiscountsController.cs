328de43bcdfd1404924649fbac2f1f0d69884a8a
Azizcan Yalçın <azizcanyalcin@gmail.com>
2025-01-04 15:39:29 +0300

    Add discount dtos and services and controller

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            this.discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCoupons()
+        {
+            var values = await discountService.GetAllCouponsAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCouponById(int id)
+        {
+            var values = await discountService.GetCouponByIdAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCoupon(CreateCouponDto createCouponDto)
+        {
+            await discountService.CreateCouponAsync(createCouponDto);
+            return Ok("Coupon created succesfully!");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCoupon(int id)
+        {
+            await discountService.DeleteCouponAsync(id);
+            return Ok("Coupon deleted succesfully!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCoupon(UpdateCouponDto updateCouponDto)
+        {
+            await discountService.UpdateCouponAsync(updateCouponDto);
+            return Ok("Coupon updated succesfully!");
+        }
+
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<GetCouponByIdDto> GetCouponByIdAsync(int id);
+        Task<List<ResultCouponDto>> GetAllCouponsAsync();
+        Task CreateCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteCouponAsync(int id);
+    }
+}
