266aa66f9b4635d0754d3f2e4edb0006cfd3335a
Azizcan Yalçın <azizcanyalcin@gmail.com>
2025-01-04 14:12:39 +0300

    Add migration.

--- Services/Discount/MultiShop.Discount/Context/DapperContext.cs
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
 
 namespace MultiShop.Discount.Context
 {
     public class DapperContext: DbContext
     {
+        public DbSet<Coupon> Coupons { get; set; }
+        private readonly IConfiguration configuration;
+        private readonly string connectionString;
 
+        public DapperContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;initial Catalog=MultiShopDiscountDb;integrated Security=true");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(connectionString);
     }
 }
